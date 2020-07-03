b57690887897d173d945b8f6c7d7042dcc7111dc
Charles Pobre <cpobre.peg@gmail.com>
2020-07-03 15:42:28 +0800

    [#1] Initial Devise gem setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :account_users, dependent: :destroy
   has_many :accounts, through: :account_users
   has_many :owned_accounts, class_name: "Account", foreign_key: :user_id, inverse_of: :owner, dependent: :restrict_with_error
