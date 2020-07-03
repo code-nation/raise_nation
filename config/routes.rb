e65e069ce2ec0d0323858358aeb88a4435f89f38
Charles Pobre <cpobre.peg@gmail.com>
2020-07-03 15:46:31 +0800

    [#1] Fix layout for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'sb_admin_docs#index'
-
   resources :sb_admin_docs, only: :index do
     get :buttons, :cards, :utilities_color, :utilities_border,
         :utilities_animation, :utilities_other, :login, :register,
         :forgot_password, :page_404, :blank, :charts, :tables,
         on: :collection
   end
+  resources :dashboard
+
+  root 'dashboard#index'
 end
