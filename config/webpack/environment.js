97c0f28c671a9ed281c678b487def306e3230bf0
Charles Pobre <cpobre.peg@gmail.com>
2020-07-13 16:56:46 +0800

    [WIP] asset fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,11 @@ module.exports = environment
 
 const webpack = require('webpack')
 
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
