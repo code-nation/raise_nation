710e53f8befad1c92d791db01a2bccdbd6bc6348
Charles Pobre <cpobre.peg@gmail.com>
2020-07-24 16:22:40 +0800

    Set default from [ci skip]

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,4 +1,6 @@
 class InvitationMailer < ApplicationMailer
+  default from: ENV['SENGRID_DEFAULT_FROM']
+
   def account_user_invitation(user:, inviter_name:, org_name:)
     @user = user
     @raw_invitation_token = user.raw_invitation_token
