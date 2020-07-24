c23c450b8fdc9614cf90d9d6940946aedbadcce5
Charles Pobre <cpobre.peg@gmail.com>
2020-07-24 16:29:29 +0800

    Update sendgrid domain [ci skip]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'raise-nation-staging.com' }
+  config.action_mailer.default_url_options = { host: ENV['SENDGRID_DOMAIN'] }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
