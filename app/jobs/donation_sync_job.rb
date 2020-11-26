9bed7474b070fa4dd44f6bad4843dabc43ff5b77
Charles Pobre <cpobrepro@gmail.com>
2020-11-26 17:44:18 +0800

    [#8] Fixed spacing and added sidekiq monitoring

--- app/jobs/donation_sync_job.rb
+++ app/jobs/donation_sync_job.rb
@@ -4,7 +4,7 @@ class DonationSyncJob < ApplicationJob
 
   def perform(workflow_id, donation_id)
     workflow = Workflow.find(workflow_id)
-    donation =  workflow.donations.find(donation_id)
+    donation = workflow.donations.find(donation_id)
 
     workflow.sync_donation!(donation)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_061719) do
+ActiveRecord::Schema.define(version: 2020_11_26_094042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_061719) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.boolean "admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
